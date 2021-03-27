c186052c6c0e7ac4767f843e2c9a66ae8a7d5af6
Mário Menezes <mario082@gmail.com>
2021-03-27 15:11:51 -0300

    html js css iniciados

---
+++ 06_02/script.js
@@ -0,0 +1,12 @@
+const PONTEIROHORA = document.querySelector("#hour");
+const PONTEIROMINUTO = document.querySelector("#minute");
+const PONTEIROSEGUNDO = document.querySelector("#second");
+
+let posicaoHr = 20;
+let posicaoMin = 130;
+let posicaoSeg = 267;
+
+PONTEIROHORA.style.transform = 
+PONTEIROMINUTO.style.transform = 
+PONTEIROSEGUNDO.style.transform = 
+
