35c825b2662332d53f1372041cdfe857a8748d2b
Mário Menezes <mario082@gmail.com>
2021-03-27 15:25:44 -0300

    capturando horário

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,11 +2,24 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
+var data = new Date();
+console.log(data);
+
+let hr = data.getHours();
+let min = data.getMinutes();
+let seg = data.getSeconds();
+console.log(`${hr}:${min}:${seg}`)
+
+
+
+console.log(hr);
+
+
 let posicaoHr = 20;
 let posicaoMin = 130;
-let posicaoSeg = 267;
+let posicaoSeg = 300;
 
-PONTEIROHORA.style.transform = 
-PONTEIROMINUTO.style.transform = 
-PONTEIROSEGUNDO.style.transform = 
+PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
 
