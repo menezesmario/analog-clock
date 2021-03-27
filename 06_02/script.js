d2a079faedeb32b5be5380143806a1824dc399f4
Mário Menezes <mario082@gmail.com>
2021-03-27 17:16:50 -0300

    relógio ok

--- 06_02/script.js
+++ 06_02/script.js
@@ -3,21 +3,25 @@ const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
 
+var data = new Date();
+console.log(data);
+
+let hr = data.getHours();
+let min = data.getMinutes();
+let seg = data.getSeconds();
+console.log(`${hr}:${min}:${seg}`)
+
+console.log(hr);
+
+let posicaoHr = (hr*360/12)+(min*(360/60)/12);
+let posicaoMin = (min*360/60)+(seg*(360/60)/60);
+let posicaoSeg = seg*360/60;
+
 function executarRelogio(){
 
-    var data = new Date();
-    console.log(data);
-
-    let hr = data.getHours();
-    let min = data.getMinutes();
-    let seg = data.getSeconds();
-    console.log(`${hr}:${min}:${seg}`)
-
-    console.log(hr);
-
-    let posicaoHr = (hr*360/12)+(min*(360/60)/12);
-    let posicaoMin = (min*360/60)+(seg*(360/60)/60);
-    let posicaoSeg = seg*360/60;
+    posicaoHr = posicaoHr+(3/360);
+    posicaoMin = posicaoMin+(6/60);
+    posicaoSeg = posicaoSeg+6;
 
     PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
     PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
