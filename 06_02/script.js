a489d0e815710241fba745e3dcc0ad358f38425a
Mário Menezes <mario082@gmail.com>
2021-03-27 16:53:37 -0300

    relógio executando

--- 06_02/script.js
+++ 06_02/script.js
@@ -2,21 +2,27 @@ const PONTEIROHORA = document.querySelector("#hour");
 const PONTEIROMINUTO = document.querySelector("#minute");
 const PONTEIROSEGUNDO = document.querySelector("#second");
 
-var data = new Date();
-console.log(data);
 
-let hr = data.getHours();
-let min = data.getMinutes();
-let seg = data.getSeconds();
-console.log(`${hr}:${min}:${seg}`)
+function executarRelogio(){
 
-console.log(hr);
+    var data = new Date();
+    console.log(data);
 
-let posicaoHr = (hr*360/12)+(min*(360/60)/12);
-let posicaoMin = (min*360/60)+(seg*(360/60)/60);
-let posicaoSeg = seg*360/60;
+    let hr = data.getHours();
+    let min = data.getMinutes();
+    let seg = data.getSeconds();
+    console.log(`${hr}:${min}:${seg}`)
 
-PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
-PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
-PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+    console.log(hr);
 
+    let posicaoHr = (hr*360/12)+(min*(360/60)/12);
+    let posicaoMin = (min*360/60)+(seg*(360/60)/60);
+    let posicaoSeg = seg*360/60;
+
+    PONTEIROHORA.style.transform = "rotate(" + posicaoHr + "deg)";
+    PONTEIROMINUTO.style.transform = "rotate(" + posicaoMin + "deg)";
+    PONTEIROSEGUNDO.style.transform = "rotate(" + posicaoSeg + "deg)";
+ 
+}
+
+var intervalo = setInterval(executarRelogio, 1000)
